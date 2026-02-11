95f0f68fb810254ed15d1d0e8dc441e3b7270bb7
Artturi <artturikinu@gmail.com>
2026-02-11 14:25:08 +0200

    oo

--- Viikko 2/Tehtava 5.py
+++ Viikko 2/Tehtava 5.py
@@ -2,7 +2,16 @@ leiviskat= int(input("Anna lieviskät"))
 naulat=int(input("Anna naulat"))
 luodit=int(input("Anna luodit"))
 
-1 leiviska = 20 naulaa
-1 naula = 32 luotia
-1 luoti = 13,3 grammaa
+#1 leiviska = 20 naulaa
+#1 naula = 32 luotia
+#1 luoti = 13,3 grammaa
 
+kokonaisluodit=(leiviskat*20*32+naulat*32+luodit)
+massa_grammoina=kokonaisluodit*13,3
+
+kilot=int (massa_grammoina // 1000)
+
+loputgrammat = massa_grammoina % 1000
+
+print(f"Massa nykymittojen mukaan:")
+print(f"{kilot} kilogrammaa ja {loputgrammat} grammmaa")
---
+++ Viikko 3/Tehtava 1.py
@@ -0,0 +1,4 @@
+pituus=int(input("Minkä pituinen kuhasi on?"))
+if pituus < 37:
+    print("laske kuhta takaisin veteen")
+    print(f"Kuhasi on {37-pituus} cm liian lyhyt ")
